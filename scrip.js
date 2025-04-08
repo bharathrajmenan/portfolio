d284ef95f238bd1d80927dedc9d8ec3215f08a5d
bharathrajmenan <brm@example.com>
2025-04-08 07:59:45 +0530

    bharath raj menan

--- scrip.js
+++ scrip.js
@@ -1,97 +1,87 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-  }
-  
-  
-  document.getElementById("butt1").onclick = function () {
-    location.href = 'https://bharathrajmenan.github.io/calculator/'
-     }
-     document.getElementById("butt1v").onclick = function () {
-        location.href = 'https://github.com/bharathrajmenan/calculator.git'
-         }
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
 
-   document.getElementById("butt2").onclick = function () {
-  location.href = 'https://bharathrajmenan.github.io/philosophyweb/'
-   }
-  
-   
-  document.getElementById("butt3").onclick = function () {
-    location.href = 'https://bharathrajmenan.github.io/Analogclock/'
-     }
-     document.getElementById("butt3v").onclick = function () {
-        location.href = 'https://github.com/bharathrajmenan/Analogclock.git'
-         }
+document.getElementById("butt1").onclick = function () {
+  location.href = "https://bharathrajmenan.github.io/calculator/";
+};
+document.getElementById("butt1v").onclick = function () {
+  location.href = "https://github.com/bharathrajmenan/calculator.git";
+};
 
-         
-  document.getElementById("butt4").onclick = function () {
-    location.href = 'https://bharathrajmenan.github.io/DigitalClock/'
-     }
-     document.getElementById("butt4v").onclick = function () {
-        location.href = 'https://github.com/bharathrajmenan/DigitalClock.git'
-         }
+document.getElementById("butt2").onclick = function () {
+  location.href = "https://bharathrajmenan.github.io/philosophyweb/";
+};
 
+document.getElementById("butt3").onclick = function () {
+  location.href = "https://bharathrajmenan.github.io/Analogclock/";
+};
+document.getElementById("butt3v").onclick = function () {
+  location.href = "https://github.com/bharathrajmenan/Analogclock.git";
+};
 
-         
-  document.getElementById("butt5").onclick = function () {
-    location.href = 'https://bharathrajmenan.github.io/meesho/'
-     }
-     document.getElementById("butt5v").onclick = function () {
-        location.href = 'https://github.com/bharathrajmenan/meesho.git'
-         }
+document.getElementById("butt4").onclick = function () {
+  location.href = "https://bharathrajmenan.github.io/DigitalClock/";
+};
+document.getElementById("butt4v").onclick = function () {
+  location.href = "https://github.com/bharathrajmenan/DigitalClock.git";
+};
 
-   document.getElementById("butt6").onclick = function () {
-    location.href = 'https://bharathrajmenan.github.io/FormValidation/'
-     }
-     document.getElementById("butt6v").onclick = function () {
-        location.href = 'https://github.com/bharathrajmenan/FormValidation.git'
-         }
+document.getElementById("butt5").onclick = function () {
+  location.href = "https://bharathrajmenan.github.io/meesho/";
+};
+document.getElementById("butt5v").onclick = function () {
+  location.href = "https://github.com/bharathrajmenan/meesho.git";
+};
 
-            
-   document.getElementById("butt7").onclick = function () {
-    location.href = 'https://bharathrajmenan.github.io/datastotable/'
-     }
-     document.getElementById("butt7v").onclick = function () {
-        location.href = 'https://github.com/bharathrajmenan/datastotable.git'
-         }
+document.getElementById("butt6").onclick = function () {
+  location.href = "https://bharathrajmenan.github.io/FormValidation/";
+};
+document.getElementById("butt6v").onclick = function () {
+  location.href = "https://github.com/bharathrajmenan/FormValidation.git";
+};
 
-         
-   document.getElementById("butt8").onclick = function () {
-    location.href = 'https://bharathrajmenan.github.io/Todolist/'
-     }
-     document.getElementById("butt8v").onclick = function () {
-        location.href = 'https://github.com/bharathrajmenan/Todolist.git'
-         }
+document.getElementById("butt7").onclick = function () {
+  location.href = "https://bharathrajmenan.github.io/datastotable/";
+};
+document.getElementById("butt7v").onclick = function () {
+  location.href = "https://github.com/bharathrajmenan/datastotable.git";
+};
 
+document.getElementById("butt8").onclick = function () {
+  location.href = "https://bharathrajmenan.github.io/Todolist/";
+};
+document.getElementById("butt8v").onclick = function () {
+  location.href = "https://github.com/bharathrajmenan/Todolist.git";
+};
 
-         document.getElementById("butt9").onclick = function () {
-            location.href = 'https://bharathrajmenan.github.io/loading_animations/'
-             }
-             document.getElementById("butt9v").onclick = function () {
-                location.href = 'https://github.com/bharathrajmenan/loading_animations.git'
-                 }
+document.getElementById("butt9").onclick = function () {
+  location.href = "https://bharathrajmenan.github.io/loading_animations/";
+};
+document.getElementById("butt9v").onclick = function () {
+  location.href = "https://github.com/bharathrajmenan/loading_animations.git";
+};
 
-                 document.getElementById("butt10").onclick = function () {
-                    location.href = 'https://bharathrajmenan.github.io/guessanum/'
-                     }
-                     document.getElementById("butt10v").onclick = function () {
-                        location.href = 'https://github.com/bharathrajmenan/guessanum.git'
-                         }
+document.getElementById("butt10").onclick = function () {
+  location.href = "https://bharathrajmenan.github.io/guessanum/";
+};
+document.getElementById("butt10v").onclick = function () {
+  location.href = "https://github.com/bharathrajmenan/guessanum.git";
+};
 
-                         document.getElementById("butt11").onclick = function () {
-                            location.href = 'https://bharathrajmenan.github.io/testimonial/'
-                             }
-                             document.getElementById("butt11v").onclick = function () {
-                                location.href = 'https://github.com/bharathrajmenan/testimonial.git'
-                                 }
+document.getElementById("butt11").onclick = function () {
+  location.href = "https://bharathrajmenan.github.io/testimonial/";
+};
+document.getElementById("butt11v").onclick = function () {
+  location.href = "https://github.com/bharathrajmenan/testimonial.git";
+};
 
-                document.getElementById("butt12").onclick = function () {
-            location.href = 'https://bharathrajmenan.github.io/nav/'
-             }
-             document.getElementById("butt12v").onclick = function () {
-                location.href = 'https://github.com/bharathrajmenan/nav.git'
-                 }
-
-         
+document.getElementById("butt12").onclick = function () {
+  location.href = "https://bharathrajmenan.github.io/nav/";
+};
+document.getElementById("butt12v").onclick = function () {
+  location.href = "https://github.com/bharathrajmenan/nav.git";
+};
